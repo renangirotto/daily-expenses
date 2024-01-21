bd3b04142affab464d6449c1821a939e1ff4d608
Renan Girotto <46816071+renangirotto@users.noreply.github.com>
2024-01-21 16:40:56 -0300

    feat: :lipstick: add main layout grid styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,17 @@
 	import '@fontsource-variable/montserrat';
 </script>
 
-<slot />
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		min-height: 100dvh;
+		padding-inline: 16px;
+		background: #f9f9fa;
+	}
+</style>
