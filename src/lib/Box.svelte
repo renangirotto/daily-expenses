befc5beb8dd642db81961261da9a19716f3b319b
Renan Girotto <46816071+renangirotto@users.noreply.github.com>
2024-01-21 16:40:08 -0300

    feat: :sparkles: add Box component

---
+++ src/lib/Box.svelte
@@ -0,0 +1,33 @@
+<script>
+	/** @type {string} */
+	export let title;
+</script>
+
+<article class="box">
+	<header class="header">{title}</header>
+	<div class="content">
+		<slot />
+	</div>
+</article>
+
+<style>
+	.box {
+		container-type: inline-size;
+		padding-inline: 16px;
+		border: 1px solid #ededf0;
+		border-radius: 8px;
+		background: #ffffff;
+	}
+
+	.content {
+		padding-block: 16px;
+	}
+
+	.header {
+		padding-block: 16px;
+		border-block-end: 1px solid #ededf0;
+		font-weight: 600;
+		font-size: 1.125rem;
+		color: #4a4a55;
+	}
+</style>
